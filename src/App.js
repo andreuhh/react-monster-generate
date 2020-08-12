444944efae20a870acc3dec0f874ceceb1f7e977
andreuhh <57524903+andreuhh@users.noreply.github.com>
2020-08-12 11:53:46 +0200

    aggiunto stile

--- src/App.js
+++ src/App.js
@@ -11,12 +11,18 @@ class App extends Component {
       monsters: [],
       searchField: ''
     };
+    // .bind() è Necessario per accedere al corretto valore di `this` all'interno della callback
+    //this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
+  }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
   }
 
   render() {
@@ -31,9 +37,10 @@ class App extends Component {
 
     return (
       <div className='App'>
+        <h1>Monster Rollodex</h1>
         <SearchBox
           placeholder='search monsters'
-          handleChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filterMonsters} />
       </div>
